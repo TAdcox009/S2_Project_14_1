ebdcdb00a7df784b52d3d037864c7fbd0e911168
Unknown <TAdcox009@west-mec.org>
2019-04-05 16:47:40 -0700

    final commit
    Finished external stylesheet, debugged code. Final commit, ready for review.

--- na_styler.js
+++ na_styler.js
@@ -30,50 +30,47 @@ window.onload = setStyles;
 function setStyles() {
       var styleNum = randInt(5);
 
-      // 6. Create an element node for the link element and set its rel attribute to “stylesheet”, its id attri- bute to “fancySheet”, and its href attribute to “na_style_num.css” where num is the value of the styleNum variable. Append the fancySheet style element to the document head, adding it to the document’s style sheets collection.
-
       var linkElem = document.createElement("link");
       linkElem.setAttribute("rel", "stylesheet");
       linkElem.setAttribute("id", "fancySheet");
       linkElem.setAttribute("href", "na_style_" + styleNum + ".css");
       document.head.appendChild(linkElem);
 
-      // 7. Create an element node named figbox for the figure element. Set its id attribute to “styleThumbs” and append it to the div element with the ID “box”.
       var figBox = document.createElement("figure");
       figBox.setAttribute("id", "styleThumbs");
       document.getElementById("box").appendChild(figBox);
 
+      for (var i = 0; i <= 4; i++) {
+            var sheetIMG = document.createElement("img");
+            sheetIMG.setAttribute("src", "na_small_" + i + ".png");
+            sheetIMG.setAttribute("alt", "na_style_" + i + ".css");
 
-      // 8. Next, populate the figure box with preview images of the five fancy style sheets. Insert a for loop with an index that goes from 0 up through 4 and each time through the loop do the following:
-      for (var i = 0; i < 4; i++) {
-            var sheetImg = document.createElement("img");
-            sheetImg.setAttribute("src", "na_small_" + i + ".png");
-            sheetImg.setAttribute("alt", "na_style_" + i + ".css");
-            sheetImg.addEventListener("click", function () {
+            sheetIMG.addEventListener("click", function (e) {
                   document.getElementById("fancySheet").setAttribute("href", e.target.alt);
             });
-            document.appendChild(sheetImg);
+            figBox.appendChild(sheetIMG);
       }
-
-
-
-
-      document.styleSheets[document.styleSheets.length - 1].insertRule(
-            "figure#styleThumbs { \
-            position: absolute; \
-            left: 0px; \
-            bottom: 0px; \
-      }", 0);
+      var thumbStyles = document.createElement("style");
+      document.head.appendChild(thumbStyles);
 
       document.styleSheets[document.styleSheets.length - 1].insertRule(
             "figure#styleThumbs { \
-            outline: pointer; \
-            opacity: 0.75; \
-      }", 1);
+                  position: absolute; \
+                  left: 0px; \
+                  bottom: 0px; \
+            }", 0);
 
       document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs { \
+                  outline: pointer; \
+                  opacity: 0.75; \
+            }", 1);
 
-      );
+      document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "figure#styleThumbs img:hover { \
+                  outline: 1px solid red; \
+                  opacity: 1.0; \
+            }", 2);
 
 }
 
